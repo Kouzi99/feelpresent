845dafcde5d68a83119158944b0915f86962ee71
Kouzi99 <kozakadam99@gmail.com>
2023-09-19 20:59:24 +0200

    mess1

--- skript.js
+++ skript.js
@@ -1,11 +1,27 @@
+function myFunction(x) {
+   x.classList.toggle("change");
+ } 
+  
    //show and hide dropdown list item on button click
    function show_hide() {
     var click = document.getElementById("list-items");
-    if (click.style.display === "none") {
-       click.style.display = "block";
+    if (click.style.display === "none" ) {
+      click.style.display = "block"; 
+      
     } else {
-       click.style.display = "none";
+       click.style.display = "none"; 
+       
     }
  }
 
+
  
+
+
+ const element = document.querySelector('.container2');
+
+ element.addEventListener('click', () => {
+     element.classList.toggle('highlighted');
+ });
+
+
